0347ea9235a06de37001e4f21f42ae7d2f1500db
Nicholas Car <nick@kurrawong.net>
2024-08-05 22:02:33 +1000

    added overview images

--- scripts/make_html.py
+++ scripts/make_html.py
@@ -6,8 +6,28 @@ REPO_DIR = Path(__file__).parent.parent.resolve()
 # initialise
 od = OntPub(ontology=REPO_DIR / "bore.ttl")
 
-# produce HTML
+# make HTML
 html = od.make_html()
 
-# or save HTML to a file
-od.make_html(destination=REPO_DIR / "bore.html")
+# post-process HTML to put in images
+# deduplicate id="bore"
+before = '''<dl>
+          <dt id="bore">bore</dt>'''
+after = '''<dl>
+          <dt id="b">bore</dt>'''
+html = html.replace(before, after)
+
+# remove title
+html = html.replace('<h1>Bore Model</h1>\n',  '')
+
+# add overview figure
+concepts_img = f'''<div id="content">
+      <h1>Bore Model</h1>
+      {open(REPO_DIR / "img" / "Concepts.svg").read()}
+      <br /><br />
+      <img src="img/Overview.png" style="width:600px;" /> 
+'''
+html = html.replace('<div id="content">', concepts_img)
+
+# write HTML to file
+open(REPO_DIR / "bore.html", "w").write(html)
