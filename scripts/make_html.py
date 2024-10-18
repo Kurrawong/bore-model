d2c29139e5287b97898b97a7c273dc2c628df3f8
Nicholas Car <nick@kurrawong.net>
2024-10-18 11:32:05 +1000

    improve HTML generation script

--- scripts/make_html.py
+++ scripts/make_html.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from pylode.profiles.ontpub import OntPub
+import re
+from textwrap import indent
 
 REPO_DIR = Path(__file__).parent.parent.resolve()
 
@@ -17,17 +19,22 @@ after = '''<dl>
           <dt id="b">bore</dt>'''
 html = html.replace(before, after)
 
-# remove title
-html = html.replace('<h1>Bore Model</h1>\n',  '')
-
 # add overview figure
-concepts_img = f'''<div id="content">
-      <h1>Bore Model</h1>
+name = re.findall(r"<h1>(.*)</h1>", html)[0]
+name_plus_img = f'''<h1>{name}</h1>
       {open(REPO_DIR / "img" / "Concepts.svg").read()}
       <br /><br />
-      <img src="img/Overview.png" style="width:600px;" /> 
-'''
-html = html.replace('<div id="content">', concepts_img)
+      <img src="img/Overview.png" style="width:600px;" />
+      '''
+html = indent(re.sub(r"<h1>(.*)</h1>", name_plus_img, html), "        ")
+
+# concepts_img = f'''<div id="content">
+#       <h1>Bore Model</h1>
+#       {open(REPO_DIR / "img" / "Concepts.svg").read()}
+#       <br /><br />
+#       <img src="img/Overview.png" style="width:600px;" />
+# '''
+# html = html.replace('<div id="content">', concepts_img)
 
 # write HTML to file
 open(REPO_DIR / "model.html", "w").write(html)
